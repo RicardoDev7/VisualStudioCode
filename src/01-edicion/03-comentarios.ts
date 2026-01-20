2dcf05f82d59309525f98a450de31cf83be19089
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-01-19 22:53:52 -0500

    feat: comentarios en linea y bloque

--- src/01-edicion/03-comentarios.ts
+++ src/01-edicion/03-comentarios.ts
@@ -8,10 +8,14 @@
 //*        Ctrl + \
 //*
 
-const a = 10;
-const b = 20;
-const c = { a, b };
 
+/*const a = 10; 
+const b = 20; */
+// Comentar en bloque con Shit+Alt+A
+
+// const c = { a, b };
+// Comentar Linea con Crtl+K + Ctrl C
+// Remover Linea con Ctrl+K + Ctrl+U
 
 const a = 30;
 const b = 40;
